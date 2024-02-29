40afb3871834574abbd23b6e4aef30ab2a784931
SimonGendre <sims.gendre@gmail.com>
2024-02-29 19:16:57 +0100

    Update hashpwd.php

--- projet/server/hashpwd.php
+++ projet/server/hashpwd.php
@@ -1,4 +1,12 @@
 <?php
+/*
+* Ce fichier ne sert uniquement à obtenir un hash à copier-coller dans la DB manuellement. 
+*   @author : Simon Gendre
+*   @version : 0.1
+*   @lastmodified : ??? pas hier
+*   @use : pas pour faire la vaisselle
+*
+*/
 if (isset($_SERVER['REQUEST_METHOD'])) {
     switch ($_SERVER['REQUEST_METHOD']) {
         case 'GET':
@@ -7,6 +15,8 @@ if (isset($_SERVER['REQUEST_METHOD'])) {
             if (isset($_GET['pwd'])) {
                 $pwd = $_GET['pwd'];
                 echo password_hash($pwd, CRYPT_SHA256);
+            } else {
+                echo "<a href='hashpwd.php?pwd=somePassword'>url example</a>";
             }
             break;
     }
